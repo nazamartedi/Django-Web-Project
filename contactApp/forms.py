49abc31d78c3b66efa9e8e1604b7ca924183b0ca
nazamartedi <nazamartedi@gmail.com>
2022-02-03 17:51:00 -0300

    ContactForm styling

--- contactApp/forms.py
+++ contactApp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Nombre', max_length=100)
-    email = forms.EmailField(label='Email')
-    content = forms.CharField(label='Contenido')
+    name = forms.CharField(label='Nombre', max_length=100, required=True)
+    email = forms.EmailField(label='Email', required=True)
+    content = forms.CharField(label='Contenido', widget=forms.Textarea())
--- contactApp/views.py
+++ contactApp/views.py
@@ -3,5 +3,6 @@ from .forms import ContactForm
 
 def contact(request):
     
+    # We need to instantiate a ContactForm object in order to show it on the template
     contact_form = ContactForm()
     return render(request, 'contact/contact.html', {'contact_form':contact_form})
